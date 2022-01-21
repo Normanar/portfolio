f649796bd630e9da1fca32a5325ecff01722238b
Arman Alimov <527c@mail.ru>
2022-01-21 21:52:38 +0600

    21 01 some new

--- src/skills/skill/SkillOne.jsx
+++ src/skills/skill/SkillOne.jsx
@@ -4,8 +4,10 @@ import style from "./SkillOne.module.css"
 export function SkillOne(props) {
     return (
         <div className={style.skillOne}>
-            <div className={style.icon}></div>
-            <h2>{props.title}</h2>
+            <div className={style.icon}>
+
+            </div>
+            <h3>{props.title}</h3>
             <span>{props.description}</span>
         </div>
     )
