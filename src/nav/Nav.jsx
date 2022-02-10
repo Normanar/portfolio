530e4409c5d037a1fcc302a5849de097753c11f4
Arman Alimov <527c@mail.ru>
2022-02-10 15:20:28 +0600

    change css

--- src/nav/Nav.jsx
+++ src/nav/Nav.jsx
@@ -5,16 +5,16 @@ import {Link} from "react-scroll"
 export function Nav() {
     return (
         <div className={style.nav}>
-            <Link activeClass={style.active} to="main" spy={true} smooth={true} duration={500} offset={-78}>
+            <Link to="main" spy={true} smooth={true} duration={500} offset={-80}>
                 MAIN
             </Link>
-            <Link activeClass={style.active} to="skills" spy={true} smooth={true} duration={500} offset={-80}>
+            <Link to="skills" spy={true} smooth={true} duration={500} offset={-80}>
                 SKILLS
             </Link>
-            <Link activeClass={style.active} to="projects" spy={true} smooth={true} duration={500} offset={-80}>
+            <Link to="projects" spy={true} smooth={true} duration={500} offset={-80}>
                 PROJECTS
             </Link>
-            <Link activeClass={style.active} to="contacts" spy={true} smooth={true} duration={500} offset={-80}>
+            <Link to="contacts" spy={true} smooth={true} duration={500} offset={-80}>
                 CONTACTS
             </Link>
         </div>
